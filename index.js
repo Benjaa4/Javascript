e88308fb4ec3251465c9d70636becff6a64b116b
Benjamin Ariza Moreira <benjaminarizamoreira01@gmail.com>
2024-11-19 20:57:34 -0300

    Clase 1

---
+++ index.js
@@ -0,0 +1,21 @@
+let nombreDelAlumno
+nombreDelAlumno = "Alejandro" /* String */
+console.log (nombreDelAlumno)
+let nombre2 
+nombre2 =  123 /* number */
+nombre2 = true /* Boolean */
+
+let Prueba = "Benjamin"
+
+console.log ("Hola" + " " + Prueba + " ," + nombreDelAlumno)
+let numero = 20
+let numero2 = 2 
+
+console.log ("Suma " +(numero + numero2))
+console.log ("resta " +(numero - numero2))
+console.log ("multiplicacion " +(numero * numero2))
+console.log ("division " +(numero / numero2))
+console.log ("division " +(numero ** numero2))
+/* operaciones matematicas */ 
+
+
