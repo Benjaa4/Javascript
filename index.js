756a40107f9b7a039c161ebf0ab421f452e8440d
Benjamin Ariza Moreira <benjaminarizamoreira01@gmail.com>
2024-11-19 23:17:16 -0300

    Inicio de calculadora

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-let nombreDelAlumno
+/* let nombreDelAlumno
 nombreDelAlumno = "Alejandro" /* String */
-console.log (nombreDelAlumno)
-let nombre2 
-nombre2 =  123 /* number */
-nombre2 = true /* Boolean */
+/* console.log (nombreDelAlumno) */
+/* let nombre2  */
+/* nombre2 =  123 */ /* number */
+/* nombre2 = true */  /* Boolean */
 
-let Prueba = "Benjamin"
+/* let Prueba = "Benjamin"
 
 console.log ("Hola" + " " + Prueba + " ," + nombreDelAlumno)
 let numero = 20
@@ -15,7 +15,14 @@ console.log ("Suma " +(numero + numero2))
 console.log ("resta " +(numero - numero2))
 console.log ("multiplicacion " +(numero * numero2))
 console.log ("division " +(numero / numero2))
-console.log ("division " +(numero ** numero2))
+console.log ("division " +(numero ** numero2)) */
 /* operaciones matematicas */ 
+
+/* Calculadora */
+let Numero1 = parseintprompt("Cual es el primer numero")
+let Numero2 = parseintprompt("Cual es el segundo numero")
+let Operacion = parseIntprompt("Que operacion quieres hacer")
+
+
 
 
