01c620b863a686c0e17dce22e744b7b5747cffc3
Benjamin Ariza Moreira <benjaminarizamoreira01@gmail.com>
2024-11-23 02:32:02 -0300

    23/11/24

--- index.js
+++ index.js
@@ -1,28 +1,20 @@
-/* let nombreDelAlumno
-nombreDelAlumno = "Alejandro" /* String */
-/* console.log (nombreDelAlumno) */
-/* let nombre2  */
-/* nombre2 =  123 */ /* number */
-/* nombre2 = true */  /* Boolean */
+ let nombreDelAlumno
+ nombreDelAlumno = "Alejandro"  /* String */
+console.log (nombreDelAlumno) 
+ let nombre2  
+ nombre2 =  123 /* number */
+ nombre2 = true /* Boolean */
 
-/* let Prueba = "Benjamin"
-
-console.log ("Hola" + " " + Prueba + " ," + nombreDelAlumno)
-let numero = 20
+  let Prueba = "Benjamin"
+ console.log ("Hola" + " " + Prueba + " ," + nombreDelAlumno)
+ let numero = 20
 let numero2 = 2 
-
-console.log ("Suma " +(numero + numero2))
-console.log ("resta " +(numero - numero2))
-console.log ("multiplicacion " +(numero * numero2))
-console.log ("division " +(numero / numero2))
-console.log ("division " +(numero ** numero2)) */
-/* operaciones matematicas */ 
+ console.log ("Suma " +(numero + numero2))
+ console.log ("resta " +(numero - numero2))
+ console.log ("multiplicacion " +(numero * numero2))
+ console.log ("division " +(numero / numero2))
+ console.log ("potencia" +(numero ** numero2)) 
+/* operaciones matematicas */
 
 /* Calculadora */
-let Numero1 = parseintprompt("Cual es el primer numero")
-let Numero2 = parseintprompt("Cual es el segundo numero")
-let Operacion = parseIntprompt("Que operacion quieres hacer")
-
-
-
 
