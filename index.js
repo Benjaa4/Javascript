c3c65aef46435acf9f5a5b990f4891378309036a
Benjamin Ariza Moreira <benjaminarizamoreira01@gmail.com>
2024-12-19 21:22:48 -0300

    correccion de link entre html y js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function login() {
     let pais = prompt("Cual es tu nacionalidad")
     let ciudad = prompt("Cual es tu ciudad de residencia actual?")
     const login_data = [nombre, parseInt(edad), pais, ciudad]
-    let confirmacion = confirm("esta informacion es correcta?" + "nombre =" + login_data[0] + " " + "edad =" + " " + login_data[1] + " " + "pais =" + login_data[2] + " " + "ciudad=" + login_data[3])
+    let confirmacion = confirm("esta informacion es correcta?" + "\nnombre =" + login_data[0] +"\nedad =" + login_data[1] + "\npais =" + login_data[2] + "\nciudad=" + login_data[3])
 
 
     if (confirmacion === true) { alert("bienvenido " + nombre + ", " + pais + ", " + ciudad) }
@@ -19,11 +19,6 @@ function login() {
 }
 
 login()
-
-
-
-
-
 
 
 
